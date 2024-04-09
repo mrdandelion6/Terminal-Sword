4bd011a9f52181d998558af262a456f17c49a32e
mrdandelion6 <faisal.shaik@mail.utoronto.ca>
2024-04-08 22:31:33 -0400

    added  move names and element names

--- A3/battle.c
+++ A3/battle.c
@@ -1,4 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <unistd.h> // fork()
+#include <errno.h> // perrror()
+#include <fcntl.h> // open(), close(), dup2()
+#include <sys/socket.h> 
+#include <netinet/in.h>
+#include <sys/select.h>
+
+char* elements[] = {"fire", "water", "wind", "blood"};
+char* reg_moves[] = {"flame slash", "water cut", "wind slice", "blood stab"};
+char* spec_moves[] = {"Wolf-Rayet star WR 102", '15,750 psi!!', "Let us not burthen our remembrance with / A heaviness thats gone.", 'holy grail.'};
+
+struct player {
+    int element;
+};
+
+int main() {
 
 
+    return 0;
+}
