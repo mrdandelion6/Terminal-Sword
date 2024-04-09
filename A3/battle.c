2ba6f292510192dcf8136fcfa0bbf835f3b37d87
mrdandelion6 <faisal.shaik@mail.utoronto.ca>
2024-04-09 08:50:58 -0400

    created initializer for dynamic integer set

--- A3/battle.c
+++ A3/battle.c
@@ -10,6 +10,7 @@
 
 #define PORT 55976 
 #define MAXSIZE 4096
+#define INITIAL_SET_SIZE 10
 
 char* elements[] = {"fire", "water", "wind", "blood"};
 char* reg_moves[] = {"flame slash", "water cut", "wind slice", "blood stab"};
@@ -19,7 +20,35 @@ struct player {
     int element;
 };
 
+typedef struct {
+    size_t capacity;
+    size_t length;
+} Dynamic_Set_Header; // the meta data for a dynamic integer set.
+
+void* set_init() { // initialize a dynamic integer set
+    int* ptr = NULL;
+    size_t size = sizeof(Dynamic_Set_Header) + INITIAL_SET_SIZE * sizeof(int);
+    Dynamic_Set_Header* h = (Dynamic_Set_Header*) malloc(size);
+
+    if (h) {
+        h->capacity = INITIAL_SET_SIZE;
+        h->length = 0;
+        ptr = (int*)(h + 1); // we skip over the meta data, and now point to the beginning of the integer array
+    }
+
+    return ptr;
+}
+
+
+void smart_select(fd_set* rd, fd_set* wr, fd_set* mrd, fd_set* mwr) {
+    // call select()
+
+    
+}
+
+
 int main() {
+
     int soc = socket(AF_INET, SOCK_STREAM, 0);
     struct sockaddr_in server_addr; 
     server_addr.sin_family = AF_INET;
@@ -41,10 +70,24 @@ int main() {
     client_addr.sin_family = AF_INET;
     __u_int client_len = sizeof(struct sockaddr_in);
 
-    int client_soc = accept(soc, (struct sockaddr*) &client_addr, &client_len);
-    if (client_soc == -1) {
-        perror("accept");
-        exit(1);
+    // use select() to avoid blocking with accept()
+    fd_set readfds;
+    FD_ZERO (&readfds);
+    fd_set writefds;
+    FD_ZERO (&writefds);
+    fd_set filtered_readfds;
+    FD_ZERO (&filtered_readfds);
+    fd_set filtered_writefds;
+    FD_ZERO (&filtered_writefds);
+
+    while (1) {
+
+        
+        int client_soc = accept(soc, (struct sockaddr*) &client_addr, &client_len);
+        if (client_soc == -1) {
+            perror("accept");
+            exit(1);
+        }
     }
 
     return 0;
